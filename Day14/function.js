ade7b420b2b1d30842042bb3f549ee43bceb50d1
Supra78900 <skpra78900@gmail.com>
2026-01-15 11:13:45 +0530

    updated

---
+++ Day14/function.js
@@ -0,0 +1,15 @@
+// function definition 
+function add(a,b){ // a and b are here paramenters
+    console.log(a+b);
+}
+// function calling 
+add(5,3); // passing arguments ( 5, 3) 
+
+function intro(name, job, location){
+    console.log("My name is "+ name)
+    console.log("I am working as "+ job)
+    console.log("in "+ location)
+}
+intro("Prashant","Full stack developer", "Bangalore")
+intro("Jully","Full stack developer", "Bangalore")
+intro("Mayur","Software Engineer", "Chennai")
